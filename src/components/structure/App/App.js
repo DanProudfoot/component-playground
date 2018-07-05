868a9720389d6ef7226a909fed705115cc5fbb8a
Dan Proudfoot <dan@kerve.co.uk>
2018-07-05 17:36:30 +0100

    first

--- src/components/structure/App/App.js
+++ src/components/structure/App/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-import logo from 'assets/images/logo.svg';
-import style from './app.module.css';
+import { Flipper, Flipped } from 'react-flip-toolkit';
+
+import style from './app.module.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div className={style.app}>
-        <header className={style.header}>
-          <img src={logo} className={style.logo} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={style.link}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		opened: false,
+		href: null
+	};
+
+	linkClick = e => {
+		e.preventDefault();
+		console.log(e.target.href);
+		this.setState({ href: e.target.href });
+	};
+
+	render() {
+		return (
+			<Flipper flipKey={this.state.opened}>
+				<div className={style.app}>
+					<div className={style.central}>
+						<h1 className={style.heading}>
+							Something really interesting
+						</h1>
+						<p className={style.para}>
+							Some kinda paragraph. It has a link in it here to{' '}
+							<Flipped flipId="link">
+								<a
+									href="http://danproudfoot.co.uk"
+									className={style.warp}
+									onClick={this.linkClick}
+								>
+									My Site
+								</a>
+							</Flipped>
+						</p>
+					</div>
+					<div className={style.external}>
+						<iframe
+							src={this.state.href || 'about:blank'}
+							title="external"
+							frameBorder="0"
+							className={style.iframe}
+						/>
+					</div>
+				</div>
+			</Flipper>
+		);
+	}
 }
 
 export default App;
